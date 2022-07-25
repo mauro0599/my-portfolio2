82d7c82ac4e5b19ae6777100767fb3d7cde47b55
mauro0599 <62065176+mauro0599@users.noreply.github.com>
2022-07-25 14:33:44 -0300

    commit

--- dist/index.379dd93c.js
+++ dist/index.379dd93c.js
@@ -955,6 +955,6 @@ const targetElements = [
     }, 
 ];
 
-},{"@parcel/transformer-js/src/esmodule-helpers.js":"ciiiV"}]},["lBB98","hD4hw"], "hD4hw", "parcelRequire6aa4")
+},{"@parcel/transformer-js/src/esmodule-helpers.js":"ciiiV"}]},["lBB98","hD4hw"], "hD4hw", "parcelRequire82ef")
 
 //# sourceMappingURL=index.379dd93c.js.map
